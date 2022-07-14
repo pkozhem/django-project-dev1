d6e10121d35e27db14ee81ae581229676b3154ec
pkozhem <pkozhem@gmail.com>
2022-07-14 13:40:29 +0300

    Initial commit

--- headproject/headproject/settings.py
+++ headproject/headproject/settings.py
@@ -132,6 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/'
+
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
