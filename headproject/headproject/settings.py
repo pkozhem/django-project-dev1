2ae70e0df8dc1dcae085f1bab13a64fa3b41676e
pkozhem <pkozhem@gmail.com>
2022-07-16 18:24:04 +0300

    Rollback personal cabinet away. Remove 'slug' and 'photo' fields in User Model

--- headproject/headproject/settings.py
+++ headproject/headproject/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'vcabinet',
     'users',
     'newsapp',
     'mainapp',
--- headproject/headproject/urls.py
+++ headproject/headproject/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', include('mainapp.urls')),
     path('news/', include('newsapp.urls')),
     path('users/', include('users.urls')),
-    path('account/', include('vcabinet.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
