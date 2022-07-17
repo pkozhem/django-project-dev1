40ffd77552635d83caa16a5d6fd0258f95460a12
pkozhem <pkozhem@gmail.com>
2022-07-17 17:25:54 +0300

    Check PATCH 0.5.1

--- headproject/headproject/settings.py
+++ headproject/headproject/settings.py
@@ -34,14 +34,12 @@ INSTALLED_APPS = [
     'users',
     'newsapp',
     'mainapp',
-    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # pip apps
 ]
 
 MIDDLEWARE = [
