c4ad19a9bfaf6c7620866566b01aa2e6d9ab1629
pkozhem <pkozhem@gmail.com>
2022-07-26 19:24:09 +0300

    Initial commit

--- headproject/newsapp/views.py
+++ headproject/newsapp/views.py
@@ -11,7 +11,6 @@ from .forms import ArticlesForm
 
 class NewsHomeView(TemplateView):
     template_name = 'newsapp/news.html'
-    model = Articles
 
     def get_context_data(self, **kwargs):
         contex = super().get_context_data(**kwargs)
